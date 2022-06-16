486094ce97b445e5116ab631ab8695d359d07d80
JBxSlash <100986504+JBxSlash@users.noreply.github.com>
2022-06-15 21:38:26 -0500

    Update bootstrapper.py

--- GAR/bootstrapper.py
+++ GAR/bootstrapper.py
@@ -6,6 +6,24 @@ from pathlib import Path
 import requests
 import random
 import time
+import sys
+import subprocess
+
+req_pkg = ["hashlib","threading","socket","pyperclip","tkinter","random","math"]
+
+def install(pkg):
+    try:
+        subprocess.check_call([sys.executable,"-m","pip","install",pkg])
+    except:
+        return False
+os.system('cls' if os.name == 'nt' else 'clear')
+for Current in range(len(req_pkg)):
+    fon = install(req_pkg[Current])
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("[" + "-" * Current + " " * (len(req_pkg)-Current) + "] Installing Required Imports...")
+    print("Trying to install " + req_pkg[Current])
+    if fon == False:
+        print("Already Installed...")
 
 
 for pop in range(11):
@@ -22,12 +40,25 @@ print("[+] Making File.py")
 REPO_DATA = random.randint(100000,999999)
 folder = ""
 path = ""
-if Path("Documents\TEST_TEXT_GAR"):
-    folder = open("Documents\TEST_TEXT_GAR\\" + str(REPO_DATA) + ".py", "a")
-    path = "Documents\TEST_TEXT_GAR\\" + str(REPO_DATA) + ".py"
+Documents = os.path.expanduser('~')  + "\\Documents\\"
+if Path(Documents + "\TEST_TEXT_GAR"):
+    try:
+        with open(Documents + "\TEST_TEXT_GAR\\" + str(REPO_DATA) + ".py", 'x') as f:
+            folder = f
+    except FileNotFoundError:
+        with open(Documents + "\\" + str(REPO_DATA) + ".py", 'a') as a:
+            folder = a
+    path = Documents + "\TEST_TEXT_GAR\\" + str(REPO_DATA) + ".py"
+    
 else:
-    folder = open("Documents\\" + str(REPO_DATA) + ".py", "a")
-    path = "Documents\\" + str(REPO_DATA) + ".py"
+    try:
+        with open(Documents + "\\" + str(REPO_DATA) + ".py", 'x') as f:
+            folder = f
+    except FileNotFoundError:
+        with open(Documents + "\\" + str(REPO_DATA) + ".py",'a') as a:
+            folder = a
+    path = Documents + "\\" + str(REPO_DATA) + ".py"
+    
 
 for pop in range(11):
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -39,6 +70,7 @@ with open(path, "w") as file:
     file.write(resp.text)
 os.system('cls' if os.name == 'nt' else 'clear')
 print("Finished: Loading file; Thanks for using JBxSlash's Github > File booststrapper!")
-os.system(path)
-
-
+stream = os.popen('"' + path +'"')
+io = stream.read()
+print("Running - " + '"' + path +'"' + " -  :  Returned: ")
+print(io)
